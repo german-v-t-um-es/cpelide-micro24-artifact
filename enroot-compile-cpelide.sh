fc356c24de1c0bb48fa83f3b73f488e19f70ae85
german-v-t-um-es <german.v.t@um.es>
2025-05-26 14:07:20 +0200

    enroot files v1

--- enroot-compile-cpelide.sh
+++ enroot-compile-cpelide.sh
@@ -7,11 +7,11 @@
 #SBATCH -c 8                                    # Request 8 CPU core
 
 # Build gem5
-enroot start --rw --mount $(pwd):$(pwd) cpelide bash -c "cd gem5_multigpu/; scons -j8 build/GCN3_X86/gem5.opt"
+enroot start --rw --mount $(pwd):$(pwd) cpelide bash -c "cd /home/germanvt/MULTIGPU/cpelide-micro24-artifact/gem5_multigpu/; scons -j8 build/GCN3_X86/gem5.opt"
 
 # Build m5ops used by benchmarks
-enroot start --rw --mount $(pwd):$(pwd) cpelide bash -c "cd gem5_multigpu/util/m5; scons -j8 build/x86/out/m5"
+enroot start --rw --mount $(pwd):$(pwd) cpelide bash -c "cd /home/germanvt/MULTIGPU/cpelide-micro24-artifact/gem5_multigpu/util/m5; scons -j8 build/x86/out/m5"
 
 # Compile square benchmark
-enroot start --rw --mount $(pwd):$(pwd) cpelide bash -c "cd gem5_multigpu/multigpu_benchmarks/square; make square_m"
+enroot start --rw --mount $(pwd):$(pwd) cpelide bash -c "cd /home/germanvt/MULTIGPU/cpelide-micro24-artifact/gem5_multigpu/multigpu_benchmarks/square; make square_m"
 
